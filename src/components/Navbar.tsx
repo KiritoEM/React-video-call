87aa2586da838e5940b6442787bb25ee115475db
kiritoEM <johankirito64@gmail.com>
2024-03-13 12:06:58 +0100

    add navbar to landing

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav
+      id="navbar"
+      className="w-full h-20 z-50 sticky top-0 px-8 flex items-center justify-between"
+    >
+      <div className="logo flex items-center gap-2">
+        <img src="/logo.png" alt="" style={{ width: "32px" }} />
+        <h4 className="text-white font-bold text-lg">videoCall</h4>
+      </div>
+      <div className="icon">
+        <img src="/menu.png" alt="" style={{ width: "25px" }} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,11 @@
+import Navbar from "../components/Navbar";
 import ThreeScene from "../components/ThreeScene";
 
 const Landing = (): JSX.Element => {
   return (
     <section id="landing" className="w-full h-screen overflow-hidden">
       <ThreeScene />
-      {/* barre de navigation */}
-      <nav id="navbar" className="w-full h-20"></nav>
+      <Navbar />
       <section
         id="landing__container"
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-97 h-auto overflow-hidden flex flex-col gap-7"
@@ -24,9 +24,9 @@ const Landing = (): JSX.Element => {
           </p>
         </div>
         <div className="button flex flex-row justify-center">
-          <button className="text-white flex items-center gap-1.5 border-2 border-green-400 rounded-xl px-4 py-2 hover:bg-green-400">
+          <button className="text-white flex items-center gap-1.5 border-2 border-green-400 rounded-xl px-4 py-2 hover:bg-green-400 bg-stone-900">
             Rejoindre un room{" "}
-            <img src="/chevron-right.png" alt="" style={{ width: "18px" }} />
+            <img src="/chevron-right.png" alt="" style={{ width: "16px" }} />
           </button>
         </div>
       </section>
